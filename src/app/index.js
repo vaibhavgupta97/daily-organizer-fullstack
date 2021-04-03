75ebd1d484b22ceb58a263093aea90f29c1c9605
vaibhav <vaibhav16feb1997@gmail.com>
2021-04-03 11:52:24 +0530

    Initialize project using Create React App

---
+++ src/app/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>Hello</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+reportWebVitals();
